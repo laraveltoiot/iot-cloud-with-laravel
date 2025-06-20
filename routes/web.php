7df09e0d09dd174d6d64aa09235f2715965e2619
Bogdan <gewaldb@gmail.com>
2025-06-20 22:02:35 +0200

    add User Management route and sidebar link, update .gitignore

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php declare(strict_types=1);
 
+use App\Livewire\Admin\UserManagement;
 use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
@@ -19,6 +20,9 @@ Route::middleware(['auth'])->group(function () {
     Route::get('settings/profile', Profile::class)->name('settings.profile');
     Route::get('settings/password', Password::class)->name('settings.password');
     Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+
+    // Admin routes
+    Route::get('admin/users', UserManagement::class)->name('admin.users');
 });
 
 require __DIR__.'/auth.php';
